a14a79d8163277b985b461e26d5101b2b8995174
Nathan Osman <nathan@quickmediasolutions.com>
2025-08-02 23:38:19 -0700

    Add IP address definitions.

---
+++ mdns.go
@@ -0,0 +1,10 @@
+package gomdns
+
+const (
+
+	// MDNSIPv4Addr is the IPv4 address used for mDNS.
+	MDNSIPv4Addr = "224.0.0.251:5353"
+
+	// MDNSIPv6Addr is the IPv6 address used for mDNS.
+	MDNSIPv6Addr = "[ff02::fb]:5353"
+)
