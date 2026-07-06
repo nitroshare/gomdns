698bb46fdb007626c6a8d96b6a03bb86cb460d5c
Nathan Osman <nathan@nathanosman.com>
2026-07-05 23:06:57 -0700

    Begin refactoring Server.

--- server/server.go
+++ server/server.go
@@ -1,122 +1,64 @@
 package server
 
 import (
-	"context"
-	"log/slog"
 	"net"
-	"sync"
-	"syscall"
 	"time"
 
-	"github.com/nitroshare/mocktime"
-	"golang.org/x/net/ipv4"
-	"golang.org/x/net/ipv6"
+	"github.com/nitroshare/gomulticast"
 )
 
-const mDNSListenAddr = ":5353"
-
-var (
-	mDNSMulticastIPv4Addr = &net.UDPAddr{
-		IP: net.ParseIP("224.0.0.251"),
-	}
-	mDNSMulticastIPv6Addr = &net.UDPAddr{
-		IP: net.ParseIP("ff02::fb"),
-	}
-	listenConfig = net.ListenConfig{
-		Control: func(network, address string, c syscall.RawConn) error {
-			return c.Control(func(fd uintptr) {
-				syscall.SetsockoptInt(
-					syscall.Handle(fd),
-					syscall.SOL_SOCKET,
-					syscall.SO_REUSEADDR,
-					1,
-				)
-			})
-		},
-	}
-)
-
-// Server sends and receives DNS messages on all available multicast
-// interfaces.
+// Server sends and receives packets on the system's multicast interfaces.
 type Server struct {
-	wg         sync.WaitGroup
-	logger     *slog.Logger
-	pconn4     *ipv4.PacketConn
-	pconn6     *ipv6.PacketConn
-	chanPacket chan<- []byte
-	chanClose  chan any
+	watcher     *gomulticast.Watcher
+	chanAdded   chan gomulticast.Interface
+	chanRemoved chan gomulticast.Interface
+	chanClosed  chan any
 }
 
 func (s *Server) run() {
-	defer s.wg.Done()
-	var (
-		tickerScan = mocktime.NewTicker(30 * time.Second)
-	)
-	defer tickerScan.Stop()
-	s.scan()
+	defer close(s.chanClosed)
+	ifaceMap := map[string]*net.Interface{}
 	for {
 		select {
-		case <-tickerScan.C:
-			s.scan()
-		case <-s.chanClose:
-			return
+		case i, ok := <-s.chanAdded:
+			if !ok {
+				return
+			}
+			ifaceMap[i.Interface().Name] = i.Interface()
+		case i, ok := <-s.chanRemoved:
+			if !ok {
+				return
+			}
+			delete(ifaceMap, i.Interface().Name)
 		}
 	}
 }
 
-// New attempts to create a new Server.
-func New(cfg *Config) (*Server, error) {
-
-	// Create the listener for IPv4 packets
-	conn4, err := listenConfig.ListenPacket(
-		context.Background(),
-		"udp4",
-		mDNSListenAddr,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create the listener for IPv6 packets
-	conn6, err := listenConfig.ListenPacket(
-		context.Background(),
-		"udp6",
-		mDNSListenAddr,
-	)
-	if err != nil {
-		conn4.Close()
-		return nil, err
-	}
-
-	// Create the server, converting the net.PacketConn to ipv4/6.PacketConn
+// New creates a new Server instance.
+func New() *Server {
 	s := &Server{
-		logger:     cfg.Logger,
-		pconn4:     ipv4.NewPacketConn(conn4),
-		pconn6:     ipv6.NewPacketConn(conn6),
-		chanPacket: cfg.ChanPacket,
-		chanClose:  make(chan any),
+		chanAdded:   make(chan gomulticast.Interface),
+		chanRemoved: make(chan gomulticast.Interface),
+		chanClosed:  make(chan any),
 	}
-	if s.logger == nil {
-		s.logger = slog.Default()
-	}
-	s.logger = s.logger.With("package", "server")
-
-	// Start the goroutines
-	s.wg.Add(3)
+	s.watcher = gomulticast.NewWatcher(
+		&gomulticast.WatcherConfig{
+			Interval:    2 * time.Minute,
+			ChanAdded:   s.chanAdded,
+			ChanRemoved: s.chanRemoved,
+		},
+	)
 	go s.run()
-	go s.read(conn4)
-	go s.read(conn6)
-
-	return s, nil
+	return s
 }
 
-// TODO: call LeaveGroup for interfaces upon shutdown
+// Send transmits a packet to all of the current multicast interfaces.
+func (s *Server) Send() {
+	//...
+}
 
 // Close shuts down the server.
 func (s *Server) Close() {
-	s.pconn6.Close()
-	s.pconn4.Close()
-	close(s.chanClose)
-	s.wg.Wait()
-	close(s.chanPacket)
+	s.watcher.Close()
+	<-s.chanClosed
 }
--- server/server_test.go
+++ server/server_test.go
@@ -1,3 +1,17 @@
 package server
 
-//...
+import (
+	"testing"
+
+	"github.com/nitroshare/gomulticast"
+	"github.com/nitroshare/gotime"
+)
+
+func TestServer(t *testing.T) {
+	gotime.Mock()
+	defer gotime.Unmock()
+	gomulticast.Mock()
+	defer gomulticast.Unmock()
+	s := New()
+	defer s.Close()
+}
